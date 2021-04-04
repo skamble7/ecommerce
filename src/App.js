b3f3fb7c2e80ab0e691af7b9f8c7634a87750050
Sandeep Kamble <kamble.sandeep@gmail.com>
2021-04-04 17:53:09 -0400

    added code for lesson2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
 import './App.css';
+import HomePage from  './homepage.component';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
 }
 
 export default App;
